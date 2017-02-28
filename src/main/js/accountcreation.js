8072e2e857894239ae879579b07d84ee91992df7
Bryce Reid <bryce.j.reid@gmail.com>
2017-02-28 14:12:58 -0500

    Account creation completed.

--- src/main/js/accountcreation.js
+++ src/main/js/accountcreation.js
@@ -1,16 +1,69 @@
 import React from 'react';
 
-export class AccountCreation extends React.Component {
+var CreateAccountComponent = React.createClass({
 
-    constructor() {
-        super();
+    // Used to initialize state
+    getInitialState () {
+        return {
+            name : "",
+            message : ""
+        }
+    },
+
+    handleNameChange (e) {
+        // Prevent following the link.
+        e.preventDefault();
+        this.setState({ name : e.target.value , message : "..."});
+    },
+
+    handleStatusChange (e) {
+        // Prevent following the link.
+        e.preventDefault();
+        this.setState({ status : e.target.value  , message : "..."});
+    },
+
+    handleSubmit(e) {
+        // Prevents reinitialization
+        e.preventDefault();
+        let name = this.state.name;
+        fetch('http://localhost:8080/accountCreation/createAccount?'
+            + 'userName=' + name, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }).then(res =>{
+            if(res.ok){
+                this.setState({message: 'Yes!'});
+            }
+            else{
+                this.setState({message: 'No...'});
+            }
+        })
+    },
+
+    render () {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        <input type="text" defaultValue={this.state.name} onChange={this.handleNameChange}/>
+                    </label>
+                    <input type="submit" value="Create Account!" />
+                </form>
+                Name: {this.state.name}
+                <br/>
+                Message: {this.state.message}
+            </div>
+        );
     }
+});
+
+export class AccountCreation extends React.Component {
 
     render() {
         return(
-            <div>
-                Account creation elements go here.
-            </div>
+            <CreateAccountComponent/>
         );
     }
 }
